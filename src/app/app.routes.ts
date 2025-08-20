fb4b8428045a8cfda78af5bce1daac9d5ec0060b
Jamer Delgado <133704939+Jd2698@users.noreply.github.com>
2025-08-19 19:42:55 -0500

    add styles to tasks table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { authGuard, authRedirectGuard } from './core/guards/';
 import { authRoutes } from './features/auth/auth-routing';
 import { projectsRoutes } from './features/projects/projects-routing';
 import { tasksRoutes } from './features/tasks/tasks-routing';
